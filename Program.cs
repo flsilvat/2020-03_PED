7f601ba6cb4209adedbc0ec644079112e21c46f8
Francisco Silva <fsilva@aeroman.com.sv>
2021-03-11 13:11:56 -0600

    Add: Program

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Counters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 9;
+
+            Console.WriteLine($"\nGiven n = {n}");
+            Console.Write("\n");
+
+            for (int i = 1; i <= n; i++){
+                for (int j = n; j > i; j--)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.Write("\n");
+            }
+            
+        }
+    }
+}
